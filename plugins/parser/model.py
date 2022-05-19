91cb2d8306397f59dbdeed3d92f3387ba2035ecc
bsimjoo <bsimjoo@gmail.com>
2022-05-19 21:30:44 +0430

    created abstract class

--- plugins/parser/model.py
+++ plugins/parser/model.py
@@ -3,15 +3,46 @@
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Iterable
+from abc import ABC, abstractmethod, abstractproperty
+from telegram import InlineKeyboardButton,ParseMode
+
+class Message:
+    pass
 
 @dataclass
-class TextMessage:
+class TextMessage(Message):
+    TEXT_MAX_LENGTH = 4096
     text: str
-    inline_keyboard: Iterable[Iterable[dict]] = None
+    parse_mode: str = ParseMode.MARKDOWN
+    inline_keyboard: Iterable[Iterable[dict]] | Iterable[Iterable[InlineKeyboardButton]] = None
+    disable_web_page_preview: bool = False
+    disable_notification: bool = False
 
-class ParserModel:
-    def __init__(self, config:dict):
+@dataclass
+class PhotoMessage(Message):
+    CAPTION_MAX_LENGTH = 200
+    photo: str
+    caption: str = None
+    parse_mode: str = ParseMode.MARKDOWN
+    inline_keyboard: Iterable[Iterable[dict]] | Iterable[Iterable[InlineKeyboardButton]] = None
+    disable_notification: bool = False
+
+class ParserModel(ABC):
+
+    def __init__(self, config):
         self.config = config
 
-    def parse_page(self, raw_data:str) -> Iterable(Post):
-        raise NotImplementedError
+    @abstractproperty
+    def db_table(self):
+        """Bot core will use this property to initialize database"""
+        pass
+
+    @abstractmethod
+    def new_posts(self) -> Iterable[Message]:
+        """Bot core will call this method after each interval to get a list of new posts"""
+        pass
+
+    @abstractmethod
+    def last_post(self) -> Iterable[Message]:
+        """The method that will be call when user sends `/last` command"""
+        pass
